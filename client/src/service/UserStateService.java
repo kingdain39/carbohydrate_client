11792b4379e67168feda39d20cdd42cf43d378ba
yerim seo <bmo2683@naver.com>
2025-12-06 14:28:42 +0900

    feature: userService 로직

--- client/src/service/UserStateService.java
+++ client/src/service/UserStateService.java
@@ -1,5 +1,33 @@
 package service;
 
-public class UserStateService {
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Consumer;
 
+
+//접속자 관리 서비스
+public class UserStateService {
+	private final Set<String> activeUsers=new HashSet<>();
+	private Consumer<Set<String>> onUserListUpdated;
+	
+	 public void setUserListListener(Consumer<Set<String>> listener) {
+	        this.onUserListUpdated = listener;
+	    }
+	 
+	 public void updateActiveUsers(Set<String> users) {
+		 this.activeUsers.clear();
+		 this.activeUsers.addAll(users);
+		 
+		 if(onUserListUpdated!=null) {
+			 onUserListUpdated.accept(new HashSet<>(activeUsers));
+		 }
+	 }
+	 
+	 public Set<String> getActiveUsers() {
+	        return new HashSet<>(activeUsers);
+	    }
+	 
+	   public boolean isUserActive(String username) {
+	        return activeUsers.contains(username);
+	    }
 }
