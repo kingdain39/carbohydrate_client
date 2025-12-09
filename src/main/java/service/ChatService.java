7269a2319c00c246baa617065bafa2292a575e16
yerim seo <bmo2683@naver.com>
2025-12-09 23:09:27 +0900

    featuer: hitory 경로 수정

--- src/main/java/service/ChatService.java
+++ src/main/java/service/ChatService.java
@@ -74,7 +74,7 @@ public class ChatService {
         // 구독 먼저
         networkService.subscribe("/topic/public", this::handlePublicMessage);
         networkService.subscribe("/topic/private/" + currentUserName, this::handleWhisperMessage);
-        networkService.subscribe("/user/queue/history", this::handleHistoryMessage);
+        networkService.subscribe("/topic/history/" + currentUserId, this::handleHistoryMessage);
         networkService.subscribe("/topic/users", this::handleUserListMessage);
 
         // 그 다음 입장 요청
